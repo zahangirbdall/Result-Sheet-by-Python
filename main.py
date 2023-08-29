21518cf4c97cdcb6088d8e3505f54765aeedee0a
Zahangir Alam <zahangireee5@gmail.com>
2023-08-29 23:14:53 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+#Grade sheet
+marks=input("Write your marks:")
+marks=int(marks)
+if marks>=80:
+    grade="A+"
+elif marks>=70:
+    grade="A"
+elif marks>=60:
+    grade="A-"
+elif marks>=50:
+    grade="B"
+elif marks>=40:
+    grade="C"
+else:
+    grade="F"
+print("Result is", grade)
